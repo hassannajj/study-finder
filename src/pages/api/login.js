fb8f666dbe8c100a464e5cee7d1aa2ba14d080c1
Noah Wang <botaow3@uci.edu>
2023-02-04 16:30:26 -0800

    login api

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -6,23 +6,26 @@ export default async function handler(req, res) {
   // prisma queries
   const {username, password} = req.body;
 
-  const user  = await prisma.user.findUnique({
+  const user = await prisma.user.findUnique({
     select: {id: true, username: true, password: true},
     where: {username}
+    // res.redirect(307, '/') // redirect page?
   });
 
   if (!user){
-    //log in fail
-    
-  }
-  else {
-    const user1 = await prisma.user.create({
+    //log in fail, create new user
+    const create_user = await prisma.user.create({
       data: {
         username: username,
         password: password,
       },
     })
   }
+  else {
+    // if user exists, redirect from login page
+    // res.redirect('307', '/') // '/' redirect to URL path
+  }
+
 
   //hash password and compare with user.passwordHash
 

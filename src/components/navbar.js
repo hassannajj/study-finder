af564ca6725e9c9ea70357a935bbf0405ae1b1cc
Athena Rubio <75556016+SpeedyDolphin@users.noreply.github.com>
2023-02-04 21:16:03 -0800

    updated navBar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import Script from 'next/script'
+
 function IconAndTitle(prop) {
     return (
         <>
@@ -34,29 +34,18 @@ function Navbar() {
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#this_is_a_useless_link" style={{ color: "white" }}>Browse by:</a>
+                        <ul class="navbar-nav ml-auto">
+
+                            <li class="nav-link zeroBottomPadding">
+                                <IconAndTitle icon="bi bi-people-fill" text="Events" link="\tabs\events"></IconAndTitle>
                             </li>
 
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Title
-                                </a>
-                                <ul class="dropdown-menu scrollable-menu" role="menu" id='titleDrop'>
-                                </ul>
-                            </li>
-                            <li class="nav-link zeroBottomPadding">
-                                <IconAndTitle icon="bi bi-people-fill" text="Groups" link="\tabs\events"></IconAndTitle>
-                            </li>
-                            <li class="nav-link zeroBottomPadding">
-                                <IconAndTitle icon="bi bi-person-plus-fill" text="Add People" link="\tabs\friends"></IconAndTitle>
-                            </li>
-                            <li class="nav-link zeroBottomPadding">
-                                <IconAndTitle icon="bi bi-plus" text="Add" link="#"></IconAndTitle>
-                            </li>
                             <li class="nav-link zeroBottomPadding">
                                 <IconAndTitle icon="bi bi-emoji-sunglasses" text="Profile" link="\tabs\profile"></IconAndTitle>
+                            </li>
+
+                            <li class="nav-link zeroBottomPadding">
+                                <IconAndTitle icon="bi bi-arrow-bar-right" text="Sign Out" link="\tabs\login"></IconAndTitle>
                             </li>
 
                         </ul>

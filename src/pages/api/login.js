fd6ab5c0eaa21e16ada9a305acc8a83eb20aade9
Noah Wang <botaow3@uci.edu>
2023-02-04 19:01:12 -0800

    api login fixes

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -9,7 +9,6 @@ export default async function handler(req, res) {
   const user = await prisma.user.findUnique({
     select: {id: true, username: true, password: true},
     where: {username}
-    // res.redirect(307, '/') // redirect page?
   });
 
   if (!user){
@@ -20,14 +19,33 @@ export default async function handler(req, res) {
         password: password,
       },
     })
+ 
+
+    // Optional logging to see the responses
+    // in the command line where next.js app is running.
+    console.log('username and password: ', username, password)
+  
+    // Guard clause checks for first and last name,
+    // and returns early if they are not found
+    if (!username || !password) {
+      // Sends a HTTP bad request error code
+      return res.status(400).json({ data: 'username or password not found' })
+    }
+  
+    // Found the name.
+    // Sends a HTTP success code
+    res.status(200).json({ data: `${username} ${password}` })
+
   }
   else {
     // if user exists, redirect from login page
-    // res.redirect('307', '/') // '/' redirect to URL path
+    res.redirect('/tabs/profile') // '/' redirect to URL path
   }
 
 
   //hash password and compare with user.passwordHash
 
-  res.json({status: "ok"})
+  //res.json({status: "ok, got a response"})
+
+  // HTTP HEADER SENT IS WHEN YOU CALL RES.JSON TWICE!!!
 }
